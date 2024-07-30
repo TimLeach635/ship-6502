1e2fd3da05096002f6bfe1dea1773b085d62b529
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2024-07-30 22:11:36 +0100

    Familiarise myself with the emulator; read from its memory

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,38 @@
+use std::io;
+use std::io::Write;
+
 use mos6502::memory::Bus;
 use mos6502::memory::Memory;
 use mos6502::instruction::Nmos6502;
 use mos6502::cpu;
 
 fn main() {
-    let zero_page_data = [56, 49];
-
     let program = [
-        // (F)irst | (S)econd
-        // .algo
-        0xa5, 0x00,       // Load from F to A
-        // .algo_
-        0x38,             // Set carry flag
-        0xe5, 0x01,       // Substract S from number in A (from F)
-        0xf0, 0x07,       // Jump to .end if diff is zero
-        0x30, 0x08,       // Jump to .swap if diff is negative
-        0x85, 0x00,       // Load A to F
-        0x4c, 0x12, 0x00, // Jump to .algo_
-        // .end
-        0xa5, 0x00,       // Load from S to A
-        0xff,
-        // .swap
-        0xa6, 0x00,       // load F to X
-        0xa4, 0x01,       // load S to Y
-        0x86, 0x01,       // Store X to F
-        0x84, 0x00,       // Store Y to S
-        0x4c, 0x10, 0x00, // Jump to .algo
+        0xa9, 0x48,         // LDA #$48 (ascii 'H')
+        0x8d, 0x00, 0x02,   // STA $0200
+        0xa9, 0x65,         // LDA #$65 (ascii 'e')
+        0x8d, 0x01, 0x02,   // STA $0201
+        0xa9, 0x6c,         // LDA #$6c (ascii 'l')
+        0x8d, 0x02, 0x02,   // STA $0202
+        0xa9, 0x6c,         // LDA #$6c (ascii 'l')
+        0x8d, 0x03, 0x02,   // STA $0203
+        0xa9, 0x6f,         // LDA #$6f (ascii 'o')
+        0x8d, 0x04, 0x02,   // STA $0204
+        0xff,               // end?
     ];
 
     let mut cpu = cpu::CPU::new(Memory::new(), Nmos6502);
 
-    cpu.memory.set_bytes(0x00, &zero_page_data);
     cpu.memory.set_bytes(0x10, &program);
     cpu.registers.program_counter = 0x10;
 
     cpu.run();
 
-    assert_eq!(7, cpu.registers.accumulator);
-    println!("acc: {}", cpu.registers.accumulator);
+    let base: u16 = 0x0200;
+    for offset in 0u16..5u16 {
+        let loc = base + offset;
+        let byte = cpu.memory.get_byte(loc);
+        print!("{}", byte as char);
+    }
+    let _ = io::stdout().flush();
 }
