7c9262e250fe2c971c2ca4a0fd0e2eeb41ce565e
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2024-08-06 11:41:55 +0100

    Switch to Bevy for rendering

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
 use std::fs::read;
 
+use bevy::prelude::*;
+use bevy::render::render_asset::RenderAssetUsages;
 use buffer_graphics_lib::prelude::NewTextPos;
 use buffer_graphics_lib::prelude::TextPos;
 use buffer_graphics_lib::prelude::LIGHT_GRAY;
 use buffer_graphics_lib::text::PixelFont;
 use buffer_graphics_lib::text::Text;
 use buffer_graphics_lib::Graphics;
+use image::DynamicImage;
+use image::RgbaImage;
+use mos6502::cpu::CPU;
 use mos6502::memory::Bus;
 use mos6502::memory::Memory;
 use mos6502::instruction::Nmos6502;
 use mos6502::cpu;
-use pixels::Pixels;
-use pixels::SurfaceTexture;
-use tao::dpi::LogicalSize;
-use tao::event::Event;
-use tao::event::KeyEvent;
-use tao::event::WindowEvent;
-use tao::event_loop::ControlFlow;
-use tao::event_loop::EventLoop;
-use tao::keyboard::KeyCode;
-use tao::window::WindowBuilder;
 
 const SCREEN_WIDTH: u32 = 800;
 const SCREEN_HEIGHT: u32 = 600;
 
-fn main() {
+#[derive(Resource)]
+struct Cpu(CPU<Memory, Nmos6502>);
+
+#[derive(Component)]
+struct Screen;
+
+fn setup_cpu(mut commands: Commands) {
+    // Load 6502 program
     let zp = match read("asm/zp.bin") {
         Ok(data) => data,
         Err(err) => {
@@ -47,82 +49,68 @@ fn main() {
         }
     };
 
+    // Initialise 6502, load program into its memory
     let mut cpu = cpu::CPU::new(Memory::new(), Nmos6502);
-
     cpu.memory.set_bytes(0x0000, &zp);
     cpu.memory.set_bytes(0x0100, &ram);
     cpu.memory.set_bytes(0x8000, &rom);
     cpu.registers.program_counter = 0x8000;
 
-    let event_loop = EventLoop::new();
-    let window = {
-        let size = LogicalSize::new(SCREEN_WIDTH as f64, SCREEN_HEIGHT as f64);
-        WindowBuilder::new()
-            .with_title("ship_6502")
-            .with_inner_size(size)
-            .with_min_inner_size(size)
-            .build(&event_loop)
-            .unwrap()
-    };
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(SCREEN_WIDTH, SCREEN_HEIGHT, surface_texture).unwrap()
-    };
+    // Initialise Bevy global CPU resource
+    commands.insert_resource(Cpu(cpu));
+}
 
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent { event, .. } => match event {
-                // Close window
-                WindowEvent::CloseRequested
-                | WindowEvent::KeyboardInput {
-                    event:
-                        KeyEvent {
-                            physical_key: KeyCode::Escape,
-                            ..
-                        },
-                    ..
-                } => {
-                    *control_flow = ControlFlow::Exit;
-                }
+fn setup_screen(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        Screen,
+        SpriteBundle::default(),
+    ));
+}
 
-                WindowEvent::Resized(size) => {
-                    if let Err(err) = pixels.resize_surface(size.width, size.height) {
-                        eprintln!("Error resizing window: {}", err);
-                        *control_flow = ControlFlow::Exit;
-                    }
-                }
-
-                _ => {}
-            },
-
-            Event::MainEventsCleared => {
-                window.request_redraw();
-            }
-
-            Event::RedrawRequested(_) => {
-                let mut buffer = pixels.frame_mut();
-                let mut graphics = Graphics::new(&mut buffer, SCREEN_WIDTH as usize, SCREEN_HEIGHT as usize).unwrap();
-
-                // get line
-                let mut line = String::new();
-                for offset in 0..100 {
-                    let byte = cpu.memory.get_byte(0x0200 + offset);
-                    match byte {
-                        0x00 => line.push(0x20 as char),  // render byte 0x00 as a blank space (0x20). A hack, for now
-                        byte => line.push(byte as char),
-                    }
-                }
-
-                let text = Text::new(&line, TextPos::cr((0, 0)), (LIGHT_GRAY, PixelFont::Standard8x10));
-                graphics.draw(&text);
-
-                pixels.render().unwrap();
-            }
-
-            _ => {}
+fn draw_screen(
+    mut cpu: ResMut<Cpu>,
+    mut screens: Query<&mut Handle<Image>, With<Screen>>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    // Draw text to buffer
+    let mut buffer: [u8; (SCREEN_WIDTH * SCREEN_HEIGHT * 4) as usize] = [0; (SCREEN_WIDTH * SCREEN_HEIGHT * 4) as usize];
+    let mut graphics = Graphics::new(&mut buffer, SCREEN_WIDTH as usize, SCREEN_HEIGHT as usize).unwrap();
+    let mut line = String::new();
+    for offset in 0..100 {
+        let byte = cpu.0.memory.get_byte(0x0200 + offset);
+        match byte {
+            0x00 => line.push(0x20 as char),  // render byte 0x00 as a blank space (0x20). A hack, for now
+            byte => line.push(byte as char),
         }
+    }
+    let text = Text::new(&line, TextPos::cr((0, 0)), (LIGHT_GRAY, PixelFont::Standard8x10));
+    graphics.draw(&text);
 
-        cpu.single_step();
-    });
+    // Load buffer into Bevy as an Image
+    let screen_image_buffer = RgbaImage::from_raw(SCREEN_WIDTH, SCREEN_HEIGHT, buffer.to_vec()).unwrap();
+    let screen_image = Image::from_dynamic(
+        DynamicImage::ImageRgba8(screen_image_buffer),
+        false,
+        RenderAssetUsages::RENDER_WORLD
+    );
+
+    let screen_image_handle = images.add(screen_image);
+
+    // Draw image to all screens
+    for mut handle in screens.iter_mut() {
+        *handle = screen_image_handle.clone_weak();
+    }
+}
+
+fn step_cpu(mut cpu: ResMut<Cpu>) {
+    cpu.0.single_step();
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup_cpu, setup_screen))
+        .add_systems(Update, (draw_screen, step_cpu))
+        .run();
 }
