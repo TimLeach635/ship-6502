e190d2d2c2b260b4aa8a749331c58d47e84c3aae
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2025-06-24 11:07:23 +0100

    Modify device tree traversal to include resolution of ports
    This involves quite extensive modification to the traversal method, as we have to now revisit the same devices on later passes if they didn't get resolved the first time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::cmp::PartialEq;
 use std::collections::VecDeque;
-use bevy::ecs::entity::EntityHashSet;
 use bevy::ecs::relationship::{Relationship, RelationshipSourceCollection};
 use bevy::prelude::*;
 
@@ -8,7 +7,8 @@ fn main() {
     App::new()
         .add_systems(Startup, (
             test_startup,
-            resolve.after(test_startup)
+            resolve.after(test_startup),
+            check_all_ports.after(resolve),
         ))
         .add_systems(Update, reset_resolutions)
         .run();
@@ -59,95 +59,147 @@ struct OutgoingConnection(Entity);
 #[relationship_target(relationship = OutgoingConnection)]
 struct IncomingConnections(Vec<Entity>);
 
+/// Represents an output port of the entire level. Traversal of the device tree begins at these.
+#[derive(Component)]
+struct LevelOutput;
+
+#[derive(Component)]
+// TODO: Too specific? Should this be ValueHolder?
+struct Port(Option<u32>);
+
 fn test_startup(mut commands: Commands) {
     // Devices
     let root = commands.spawn((
         Device {
             kind: DeviceKind::Empty,
         },
-        Name("Root".to_string()),
+        Name("Root".to_owned()),
     )).id();
     let branch_1 = commands.spawn((
         Device {
             kind: DeviceKind::Empty,
         },
-        Name("Branch 1".to_string()),
+        Name("Branch 1".to_owned()),
     )).id();
     let branch_2 = commands.spawn((
         Device {
             kind: DeviceKind::Empty,
         },
-        Name("Branch 2".to_string()),
+        Name("Branch 2".to_owned()),
     )).id();
     let leaf_1 = commands.spawn((
         Device {
             kind: DeviceKind::Empty,
         },
-        Name("Leaf 1".to_string()),
+        Name("Leaf 1".to_owned()),
     )).id();
     let leaf_2 = commands.spawn((
         Device {
             kind: DeviceKind::Empty,
         },
-        Name("Leaf 2".to_string()),
+        Name("Leaf 2".to_owned()),
     )).id();
     let leaf_3 = commands.spawn((
         Device {
             kind: DeviceKind::Empty,
         },
-        Name("Leaf 3".to_string()),
+        Name("Leaf 3".to_owned()),
     )).id();
     
     // Output ports
     let root_o1 = commands.spawn((
         OutputPortOf(root),
-        Name("Root.o1".to_string()),
+        Port(None),
+        Name("Root.o1".to_owned()),
     )).id();
     let root_o2 = commands.spawn((
         OutputPortOf(root),
-        Name("Root.o2".to_string()),
+        Port(None),
+        Name("Root.o2".to_owned()),
     )).id();
     let branch1_o1 = commands.spawn((
         OutputPortOf(branch_1),
-        Name("Branch 1.o1".to_string()),
+        Port(None),
+        Name("Branch 1.o1".to_owned()),
     )).id();
     let branch1_o2 = commands.spawn((
         OutputPortOf(branch_1),
-        Name("Branch 1.o2".to_string()),
+        Port(None),
+        Name("Branch 1.o2".to_owned()),
     )).id();
     let branch2_o1 = commands.spawn((
         OutputPortOf(branch_2),
-        Name("Branch 2.o1".to_string()),
+        Port(None),
+        Name("Branch 2.o1".to_owned()),
     )).id();
     let branch2_o2 = commands.spawn((
         OutputPortOf(branch_2),
-        Name("Branch 2.o2".to_string()),
+        Port(None),
+        Name("Branch 2.o2".to_owned()),
+    )).id();
+    let leaf1_o1 = commands.spawn((
+        OutputPortOf(leaf_1),
+        Port(None),
+        Name("Leaf 1.o1".to_owned()),
+    )).id();
+    let leaf2_o1 = commands.spawn((
+        OutputPortOf(leaf_2),
+        Port(None),
+        Name("Leaf 2.o1".to_owned()),
+    )).id();
+    let leaf3_o1 = commands.spawn((
+        OutputPortOf(leaf_3),
+        Port(None),
+        Name("Leaf 3.o1".to_owned()),
+    )).id();
+    
+    // Level outputs
+    let out_1 = commands.spawn((
+        LevelOutput,
+        Port(None),
+        Name("Level output 1".to_owned()),
+    )).id();
+    let out_2 = commands.spawn((
+        LevelOutput,
+        Port(None),
+        Name("Level output 2".to_owned()),
+    )).id();
+    let out_3 = commands.spawn((
+        LevelOutput,
+        Port(None),
+        Name("Level output 3".to_owned()),
     )).id();
     
     // Input ports
     let branch1_i1 = commands.spawn((
         InputPortOf(branch_1),
-        Name("Branch 1.i1".to_string()),
+        Port(None),
+        Name("Branch 1.i1".to_owned()),
     )).id();
     let branch2_i1 = commands.spawn((
         InputPortOf(branch_2),
-        Name("Branch 2.i1".to_string()),
+        Port(None),
+        Name("Branch 2.i1".to_owned()),
     )).id();
     let leaf1_i1 = commands.spawn((
         InputPortOf(leaf_1),
-        Name("Leaf 1.i1".to_string()),
+        Port(None),
+        Name("Leaf 1.i1".to_owned()),
     )).id();
     let leaf2_i1 = commands.spawn((
         InputPortOf(leaf_2),
-        Name("Leaf 2.i1".to_string()),
+        Port(None),
+        Name("Leaf 2.i1".to_owned()),
     )).id();
     let leaf2_i2 = commands.spawn((
         InputPortOf(leaf_2),
-        Name("Leaf 2.i2".to_string()),
+        Port(None),
+        Name("Leaf 2.i2".to_owned()),
     )).id();
     let leaf3_i1 = commands.spawn((
         InputPortOf(leaf_3),
-        Name("Leaf 3.i1".to_string()),
+        Port(None),
+        Name("Leaf 3.i1".to_owned()),
     )).id();
     
     // Connections
@@ -157,64 +209,66 @@ fn test_startup(mut commands: Commands) {
     commands.entity(branch1_o2).insert(OutgoingConnection(leaf2_i1));
     commands.entity(branch2_o1).insert(OutgoingConnection(leaf2_i2));
     commands.entity(branch2_o2).insert(OutgoingConnection(leaf3_i1));
+    commands.entity(leaf1_o1).insert(OutgoingConnection(out_1));
+    commands.entity(leaf2_o1).insert(OutgoingConnection(out_2));
+    commands.entity(leaf3_o1).insert(OutgoingConnection(out_3));
 }
 
 fn resolve(
-    q_root_devices: Query<Entity, (With<Device>, Without<InputPorts>)>,
-    q_devices: Query<(&Name, Option<&OutputPorts>), With<Device>>,
-    q_output_ports: Query<&OutgoingConnection, With<OutputPortOf>>,
-    q_input_ports: Query<&InputPortOf, With<IncomingConnections>>,
+    q_device_entities: Query<Entity, With<Device>>,
+    q_devices: Query<(Option<&InputPorts>, Option<&OutputPorts>), With<Device>>,
+    mut q_input_ports: Query<&mut Port, (Or<(With<InputPortOf>, With<IncomingConnections>)>, Without<OutgoingConnection>)>,
+    mut q_output_ports: Query<(&mut Port, &OutgoingConnection), (With<OutputPortOf>, Without<IncomingConnections>)>,
 ) {
-    // Start at the devices that don't have any input ports, as it is these devices
-    //  whose behaviour is known right away
     let mut device_queue: VecDeque<Entity> = VecDeque::new();
-    let mut visited = EntityHashSet::new();
-    device_queue.extend(q_root_devices);
-    visited.extend(q_root_devices);
-    
-    // After we have traversed the graph starting at the input-less devices, if there are
-    //  any devices remaining that we haven't visited, then we have a problem.
-    // Note to self: perhaps it's fine to have devices that can't resolve? Like, if they
-    //  have a device that's just disconnected from all the others, it could just sit there
-    //  in an unresolved state. Maybe this is okay? Maybe there are only a subset of devices
-    //  whose resolution is required each step. Maybe none of them are, and the puzzles are
-    //  designed to have special ports that input and output.
-    // Shit, maybe that's how this should work. We don't start at input-less devices, we
-    //  have some ports that aren't attached to any device, and they feed data into the system,
-    //  following through until they hit an input port (which is an output of the whole system)
-    //  that isn't attached to any device.
-    // Of course, I could actually implement that by having special devices that own the ports
-    //  that are like "pseudo-devices" that don't visually appear in the game.
-    // Fuck, it's hard implementing this when I don't even know what the puzzles will look like.
-    // Maybe I need to, you know, actually design the game before making it?
+    device_queue.extend(q_device_entities);
+
     while let Some(device) = device_queue.pop_front() {
-        if let Ok((
-            Name(name),
-            outputs_option
-        )) = q_devices.get(device) {
-            println!("Visiting device \"{}\"", name);
-            
-            // Get all output ports of this device
-            if let Some(outputs) = outputs_option {
-                for output_entity in outputs.iter() {
-                    if let Ok(connection) = q_output_ports.get(output_entity) {
-                        let input_entity = connection.get();
-                        if let Ok(input) = q_input_ports.get(input_entity) {
-                            let connected_device = input.get();
-                            if visited.add(connected_device) {
-                                device_queue.push_back(connected_device);
-                            }
-                        } else {
-                            println!("Input port not part of a device");
-                        }
-                    } else {
-                        println!("Output port not connected to anything");
-                    }
+        let (inputs_opt, outputs_opt) = q_devices.get(device)
+            .expect("Device should always be in query");
+
+        // We can resolve this device if all the inputs are known.
+        // (For future - maybe we can even if some of them aren't!)
+        // When we do that, we should set the values of the outputs accordingly,
+        // to allow other devices to also resolve.
+        if let Some(inputs) = inputs_opt {  // If no inputs then it always resolves
+            for input_entity in inputs.iter() {
+                let input_port = q_input_ports.get(input_entity)
+                    .expect("Should not have an input port without a Port component");
+                if input_port.0.is_none() {
+                    // At least one of the input ports of this device has no value,
+                    // so (for now!) we assume we cannot resolve.
+                    device_queue.push_back(device);
+                    continue;
                 }
-            } else {
-                println!("  Device \"{}\" has no output ports", name);
             }
         }
+
+        // If we reach this point, all the input ports have known values
+        // TODO: Actually perform device-specific processing of the inputs
+        // For now, just set all the output ports of this device to 0.
+        // Note - we arbitrarily decide that it is the job of the output ports to pass
+        // their value to their connected input ports, rather than the other way round.
+        // This is arbitrary, and it could be either way, but we have to be consistent!
+        if let Some(outputs) = outputs_opt {
+            for output_entity in outputs.iter() {
+                let (mut output_port, outgoing_connection) = q_output_ports.get_mut(output_entity)
+                    .expect("Should not have an output port without a Port component");
+                let mut connected_input_port = q_input_ports.get_mut(outgoing_connection.get())
+                    .expect("Should not have an input port without a Port component");
+                let value: u32 = 0;  // This will be what changes based on the actual device
+                output_port.0 = Some(value);
+                connected_input_port.0 = Some(value);
+            }
+        }
+    }
+}
+
+fn check_all_ports(q_ports: Query<&Port>) {
+    if q_ports.iter().any(|port| port.0.is_none()) {
+        println!("Some ports have not been resolved");
+    } else {
+        println!("All ports have been resolved");
     }
 }
 
