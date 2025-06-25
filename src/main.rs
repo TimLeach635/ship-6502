52e8d930ccffe04d619d225b50705a4f3a2eaa57
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2025-06-25 11:03:43 +0100

    Window "hello world": spawn a window and draw things to it

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,33 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+    
+    let circle = meshes.add(Circle::new(50.0));
+    let colour = Color::hsl(1.0, 0.95, 0.7);
+    
+    commands.spawn((
+        Mesh2d(circle),
+        MeshMaterial2d(materials.add(colour)),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+    ));
+    commands.spawn((
+        Text::new("Hello, world!"),
+        Node {
+            position_type: PositionType::Absolute,
+            top: Val::Px(12.0),
+            left: Val::Px(12.0),
+            ..default()
+        },
+    ));
 }
