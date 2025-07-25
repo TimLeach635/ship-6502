89632efe77c499f1e44885f0c8762f7a62aa930a
TimLeach635 <4410524+TimLeach635@users.noreply.github.com>
2025-07-25 16:55:30 +0000

    Build: (327e5a1) Update patch number for release

---
+++ ship-6502_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf1949564f2720382: (a: number, b: number, c: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h91c69d2f4cb2aac6: (a: number, b: number) => void;
+export const closure5159_externref_shim: (a: number, b: number, c: any) => void;
+export const closure5164_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hea813550a9d2a95d: (a: number, b: number) => void;
+export const closure119573_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
